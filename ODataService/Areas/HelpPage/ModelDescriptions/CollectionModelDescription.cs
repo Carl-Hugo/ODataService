cf06137b7eaa9cf5c62acf00119ffdcb72551fd4
Carl-Hugo Marcotte <carlhugo@gmail.com>
2016-08-15 21:03:26 -0400

    Add project files.

---
+++ ODataService/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ODataService.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ODataService/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace ODataService.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
